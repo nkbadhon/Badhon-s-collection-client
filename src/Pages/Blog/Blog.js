f6acdcf0aa6d42859ffbda38a16a90fd2b9c603d
nkbadhon <badhon7574@gmail.com>
2022-11-10 17:47:17 +0600

    Blog MOD

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,6 +5,11 @@ const Blog = () => {
     return (
 
         <div>
+            <div className=' '>
+                <h4><strong>Question: </strong></h4>
+                <p> <strong>Answer: </strong></p>
+            </div>
+
 
         </div>
 
