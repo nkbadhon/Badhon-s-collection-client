5d0ae317f4138329289c3edaf8e84418c256ab97
nkbadhon <badhon7574@gmail.com>
2022-11-10 17:48:39 +0600

    Blog MOD3

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -6,8 +6,8 @@ const Blog = () => {
 
         <div>
             <div className=' '>
-                <h4><strong>Question: </strong></h4>
-                <p> <strong>Answer: </strong></p>
+                <h4><strong>Question: </strong>What are the difference between SQL and NoSQL?</h4>
+                <p> <strong>Answer: </strong>SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
             </div>
 
 
